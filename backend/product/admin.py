ae332e580b4973654e5f5b0fe390e93ed1522469
umtdemr <umitde692@gmail.com>
2021-08-29 19:38:53 +0300

    added product model

--- backend/product/admin.py
+++ backend/product/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "price",
+    )
+    search_fields = ("name",)
--- backend/product/models.py
+++ backend/product/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Product(models.Model):
+    """
+    A model for storing product data that scraped from the source.
+    """
+
+    name = models.CharField(
+        _("Product Name"),
+        max_length=255,
+    )
+    image_link = models.URLField(
+        _("Product Image Link"),
+    )
+    price = models.PositiveIntegerField(_("Product Price"))
+
+    class Meta:
+        indexes = [models.Index(fields=["name"], name="product_name_idx")]
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self) -> str:
+        return f"<Product {self.id} {self.name}>"
