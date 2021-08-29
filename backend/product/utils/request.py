15dff416e1172fb8654a6522b83918a11edceea9
umtdemr <umitde692@gmail.com>
2021-08-29 21:22:47 +0300

    added util for getting request content with aiohttp

--- backend/product/utils/request.py
+++ backend/product/utils/request.py
@@ -1,6 +1,16 @@
-import requests
+import asyncio
+import aiohttp
 from bs4 import BeautifulSoup
 
 
 def get_url_with_soup(url: str):
-    pass
+    loop = asyncio.get_event_loop()
+    soup = loop.run_until_complete(_get_url_with_soup(url))
+    return soup
+
+
+async def _get_url_with_soup(url: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            html = await response.text()
+            return BeautifulSoup(html, "html.parser")
