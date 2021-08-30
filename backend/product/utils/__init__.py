88cbf953d6d926c499ed8f4532ae9378383434a2
umtdemr <umitde692@gmail.com>
2021-08-30 14:59:03 +0300

    configuring product add feature

--- backend/product/utils/__init__.py
+++ backend/product/utils/__init__.py
@@ -1,3 +1,5 @@
+from django.utils.translation import ugettext_lazy as _
+
 from rest_framework.exceptions import ValidationError
 
 from product.models import Product
@@ -5,5 +7,13 @@ from product.utils.scraping import scrape_content
 
 
 def create_product_with_url(url: str) -> Product:
-    product_data = scrape_content(url)
-    print(product_data)
+    try:
+        product_data = scrape_content(url)
+        return Product.objects.create(
+            name=product_data.name,
+            image=product_data.image_link,
+            price=product_data.price,
+        )
+
+    except Exception as e:
+        raise ValidationError(_("Error: %s" % (e)))
